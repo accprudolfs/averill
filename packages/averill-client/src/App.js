9da8c732ae39ad78da15407afb786b7b055b25c5
hedgethehog <micko.eleonora@gmail.com>
2021-11-18 23:40:29 +0200

    sets routing

--- packages/averill-client/src/App.js
+++ packages/averill-client/src/App.js
@@ -1,70 +1,21 @@
 import React from 'react'
-import { Provider, useSelector, useDispatch } from 'react-redux'
+import { Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import { store } from './store'
-import { increment } from './store/slice'
-
-function GlobalCounter() {
-  const count = useSelector(state => state.counter.value)
-  const dispatch = useDispatch()
-
-  return (
-    <div
-      onClick={() => dispatch(increment())}
-    >{`app global click count is: ${count} 🚀`}</div>
-  )
-}
-
-function LocalCounter() {
-  const [count, setCount] = React.useState(0)
-
-  return (
-    <div
-      onClick={() => setCount(c => c + 1)}
-    >{`app local click count is: ${count} 👍`}</div>
-  )
-}
-
-async function fetchJson(url, init = {}) {
-  const res = await fetch(url, { ...init })
-
-  if (!res.ok) {
-    throw res
-  }
-
-  return res.json()
-}
-
-function RemoteData() {
-  const [data, setData] = React.useState()
-  const [loading, setLoading] = React.useState(true)
-  const [error, setError] = React.useState()
-
-  React.useEffect(() => {
-    fetchJson('http://localhost:3000/test')
-      .then(res => setData(res))
-      .catch(e => setError(e))
-      .finally(() => setLoading(false))
-  }, [])
-
-  if (loading) {
-    return <div>...loading</div>
-  }
-
-  if (error) {
-    return <div>{`Error: ${error.message} 🙈`}</div>
-  }
-
-  return <div>{`${data.message} 🤘`}</div>
-}
+import AllFarmsView from './views/AllFarmsView.jsx'
+import LoginView from './views/LoginView.jsx'
+import SignupView from './views/SignupView.jsx'
+import MyFarmView from './views/MyFarmView.jsx'
 
 export default function App() {
   return (
     <Provider store={store}>
-      <div className="flex flex-col justify-center items-center h-screen">
-        <GlobalCounter />
-        <LocalCounter />
-        <RemoteData />
-      </div>
+      <Routes>
+        <Route path="/" element={<AllFarmsView />} />
+        <Route path="login" element={<LoginView />} />
+        <Route path="signup" element={<SignupView />} />
+        <Route path="myfarm" element={<MyFarmView />} />
+      </Routes>
     </Provider>
   )
 }
---
+++ packages/averill-client/src/views/AllFarmsView.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../components/Header.jsx'
+
+export default function AllFarmsView() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
