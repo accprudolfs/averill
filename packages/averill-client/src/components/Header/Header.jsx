5b738e6633c4a025654d5bc40a87d70deb250677
hedgethehog <micko.eleonora@gmail.com>
2021-11-25 16:28:38 +0200

    updates navigation links' markup

--- packages/averill-client/src/components/Header/Header.jsx
+++ packages/averill-client/src/components/Header/Header.jsx
@@ -9,30 +9,38 @@ export default function NavBar() {
   const { token: isLoggedIn } = useSelector(state => state.user)
   const location = useLocation()
 
+  const loggedInNavItems = [
+    { title: 'Explore Farms', path: '/' },
+    { title: 'My Farm', path: '/myfarm' },
+  ]
+
+  const loggedOutNavItems = [
+    { title: 'Login', path: '/login' },
+    { title: 'Sign Up', path: '/signup' },
+    { title: 'Explore Farms', path: '/' },
+  ]
+
   return (
     <div className="flex justify-between items-center px-10 bg-light-bg-green">
       <Logo />
       <nav className="flex h-16 space-x-10">
         {isLoggedIn ? (
           <>
-            {location.pathname !== '/' && (
-              <NavItem title="Explore Farms" to="/" />
-            )}
-            {location.pathname !== '/myfarm' && (
-              <NavItem title="My Farm" to="/myfarm" />
+            {loggedInNavItems.map(
+              ({ path, title }) =>
+                location.pathname !== path && (
+                  <NavItem title={title} to={path} />
+                ),
             )}
             <LogoutBtn />
           </>
         ) : (
           <>
-            {location.pathname !== '/login' && (
-              <NavItem title="Login" to="/login" />
-            )}
-            {location.pathname !== '/signup' && (
-              <NavItem title="Sign Up" to="/signup" />
-            )}
-            {location.pathname !== '/' && (
-              <NavItem title="Explore Farms" to="/" />
+            {loggedOutNavItems.map(
+              ({ path, title }) =>
+                location.pathname !== path && (
+                  <NavItem title={title} to={path} />
+                ),
             )}
           </>
         )}
