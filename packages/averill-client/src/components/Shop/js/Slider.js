22f89e75e90915df8f3ea2959b68c6f536f2797f
Leonids <you@example.com>
2021-11-26 12:34:08 +0200

    Slider Update

--- packages/averill-client/src/components/Shop/js/Slider.js
+++ packages/averill-client/src/components/Shop/js/Slider.js
@@ -6,11 +6,14 @@
 // TODOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
 // TODOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
 // DO NOT REPEAT YOURSELF HEELLOOO
-let indexzz = 1
-let oldIndex = 1
-
-export function Shownext() {
-  let howMany = 4
+let FirstElement = 1
+let LastELEMENT = 1
+// How many is set in css file TODOOO
+let howMany = null
+let total = null
+// TODO
+function common() {
+  howMany = 4
   if (document.body.offsetWidth < 1000) {
     howMany = 2
   }
@@ -20,70 +23,58 @@ export function Shownext() {
   }
 
   const slides = document.getElementsByClassName('Shop-cell')
-  const total = slides.length
+  total = slides.length
 
-  oldIndex = indexzz
-  indexzz = indexzz + howMany
-
-  if (indexzz > total) {
-    indexzz = 1
+  // HIDE ALL
+  for (let i = 1; i <= total; i++) {
+    document.querySelector(`.Shop-cell:nth-child(${i})`).style.display = 'none'
   }
+}
 
-  if (oldIndex > total) {
-    oldIndex = 1
+export function Shownext() {
+  // common Fun TODO
+  common()
+
+  // NEW FIRST ELement
+  FirstElement = FirstElement + howMany
+
+  if (FirstElement > total) {
+    FirstElement = 1
   }
 
   for (let i = 0; i < howMany; i++) {
-    let gg = oldIndex + i
-    if (gg > total) {
-      gg = 1 + i
+    let nextEL = FirstElement + i
+    if (nextEL > total) {
+      nextEL = 1 + i
     }
-    document.querySelector(`.Shop-cell:nth-child(${gg})`).style.opacity = '0'
-    document.querySelector(`.Shop-cell:nth-child(${gg})`).style.display = 'none'
+    document.querySelector(`.Shop-cell:nth-child(${nextEL})`).style.display =
+      'block'
     document
-      .querySelector(`.Shop-cell:nth-child(${gg})`)
-      .classList.remove('fade')
-    let vv = indexzz + i
-    if (vv > total) {
-      vv = 1 + i
-    }
-    document.querySelector(`.Shop-cell:nth-child(${vv})`).style.display =
-      'block'
-    document.querySelector(`.Shop-cell:nth-child(${vv})`).style.display =
-      'block'
-    document.querySelector(`.Shop-cell:nth-child(${vv})`).classList.add('fade')
+      .querySelector(`.Shop-cell:nth-child(${nextEL})`)
+      .classList.add('fade')
+    LastELEMENT = FirstElement + i
   }
 }
 
 export function ShowPrew() {
-  let howMany = 4
-  if (document.body.offsetWidth < 1000) {
-    howMany = 2
-  }
+  // common Fun TODO
+  common()
 
-  if (document.body.offsetWidth < 600) {
-    howMany = 1
-  }
-
-  const slides = document.getElementsByClassName('Shop-cell')
-  const total = slides.length
-  indexzz = indexzz - howMany
-  if (indexzz < 1) {
-    indexzz = total
-  }
-
-  for (let i = 1; i <= total; i++) {
-    document.querySelector(`.Shop-cell:nth-child(${i})`).style.display = 'none'
+  LastELEMENT = LastELEMENT - howMany
+  if (LastELEMENT < 1) {
+    LastELEMENT = total
   }
 
   for (let i = 0; i < howMany; i++) {
-    let vv = indexzz - i
-    if (indexzz < 1) {
-      vv = total
+    let PrevEL = LastELEMENT - i
+    if (PrevEL < 1) {
+      PrevEL = total
     }
-    document.querySelector(`.Shop-cell:nth-child(${vv})`).style.display =
+    document.querySelector(`.Shop-cell:nth-child(${PrevEL})`).style.display =
       'block'
-    document.querySelector(`.Shop-cell:nth-child(${vv})`).style.opacity = '1'
+    document.querySelector(`.Shop-cell:nth-child(${PrevEL})`).style.opacity =
+      '1'
+    FirstElement = LastELEMENT - i
   }
 }
 
@@ -91,22 +82,8 @@ export function onResize() {
   window.addEventListener(
     'resize',
     function (event) {
-      let howMany = 4
-      if (document.body.offsetWidth < 1000) {
-        howMany = 2
-      }
-
-      if (document.body.offsetWidth < 600) {
-        howMany = 1
-      }
-
-      const slides = document.getElementsByClassName('Shop-cell')
-      const total = slides.length
-
-      for (let i = 1; i <= total; i++) {
-        document.querySelector(`.Shop-cell:nth-child(${i})`).style.display =
-          'none'
-      }
+      // common Fun TODO
+      common()
 
       for (let i = 1; i <= howMany; i++) {
         document.querySelector(`.Shop-cell:nth-child(${i})`).style.display =
@@ -116,8 +93,8 @@ export function onResize() {
           .classList.add('fade')
       }
 
-      indexzz = 1
-      oldIndex = 1
+      FirstElement = 1
+      LastELEMENT = 1
     },
     true,
   )
