e72350b0a524ed7aca2ff51d87636f728c942393
hedgethehog <micko.eleonora@gmail.com>
2021-11-29 17:15:09 +0200

    updates error popup styles

--- packages/averill-client/src/components/Error/Error.jsx
+++ packages/averill-client/src/components/Error/Error.jsx
@@ -12,7 +12,17 @@ export default function Error({ name, visible }) {
       {show && (
         <div
           onClick={handleClick}
-          className="absolute top-1/4 left-1/2 transform -translate-x-1/2 p-4 bg-error hover:bg-opacity-85"
+          className="
+            absolute 
+            top-1/4 
+            left-1/2 
+            transform 
+            -translate-x-1/2 
+            p-4 
+            bg-error 
+            hover:bg-opacity-80
+            cursor-pointer
+          "
         >
           <p className="text-white">
             {name} unsuccessfull, please try again later &#9587;
