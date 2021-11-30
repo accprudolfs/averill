6f6f0c7d16dc8ec003ee6d219b45210923d336d1
Kristaps <azorien@gmail.com>
2021-11-30 14:15:22 +0200

    Add refined plant images in SVG format

--- packages/averill-client/src/components/Plant/plants.js
+++ packages/averill-client/src/components/Plant/plants.js
@@ -1,12 +1,23 @@
 import none from './plants/none.png'
-import potato2 from './plants/potato_2.png'
+import potato0 from './plants/potato_0.svg'
+import potato1 from './plants/potato_1.svg'
+import potato2 from './plants/potato_2.svg'
+import carrot0 from './plants/carrot_0.svg'
+import carrot1 from './plants/carrot_1.svg'
+import carrot2 from './plants/carrot_2.svg'
+import banana0 from './plants/banana_0.svg'
+import banana1 from './plants/banana_1.svg'
+import banana2 from './plants/banana_2.svg'
+import apple0 from './plants/apple_0.svg'
+import apple1 from './plants/apple_1.svg'
+import apple2 from './plants/apple_2.svg'
 
 const plants = {
   none: [none],
-  potato: [potato2, potato2, potato2],
-  carrot: [potato2, potato2, potato2],
-  apple: [potato2, potato2, potato2],
-  banana: [potato2, potato2, potato2],
+  potato: [potato0, potato1, potato2],
+  carrot: [carrot0, carrot1, carrot2],
+  apple: [apple0, apple1, apple2],
+  banana: [banana0, banana1, banana2],
 }
 
 export default plants
