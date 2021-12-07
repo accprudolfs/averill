ea857c1915db247c10d82dfa123339e871be5687
alla-andronova <73844586+alla-andronova@users.noreply.github.com>
2021-12-07 12:28:07 +0200

    adds water and harvest endpoint (#32)

--- packages/averill-server/src/routes/api/farms.js
+++ packages/averill-server/src/routes/api/farms.js
@@ -6,10 +6,8 @@ const farmController = require('../../controllers/farm')
 
 router.get('/myFarm', authMiddleware, farmController.getUserFarm)
 router.post('/plant', authMiddleware, farmController.insertPlant)
+router.post('/water', authMiddleware, farmController.waterPlant)
+router.post('/harvest', authMiddleware, farmController.harvestPlant)
 // router.get('/allFarms',authMiddleware, farmController.getAllUsersFarms);
 
 module.exports = router
-
-if (false) {
-  console.log('dsdas')
-}
