6185925cb4fd24af18beeb153b26710eb9b3c935
Leonids <leonid.90@inbox.ru>
2021-12-14 22:43:55 +0200

    shop slider code update , no query selectors , use clxs

--- packages/averill-client/src/components/Shop/components/Trader.jsx
+++ packages/averill-client/src/components/Shop/components/Trader.jsx
@@ -1,33 +1,30 @@
+// import React from 'react'
+import clsx from 'clsx'
 import React from 'react'
 
-function Trader(props) {
-  // Hide Trader Speech
-  function TraderSpeechHide() {
-    const traderSpeech = document.getElementById('traderSpeech')
-    traderSpeech.style.display = 'none'
-  }
+export default function Trader(props) {
   return (
     <div className="trader-area">
       <div
-        id="traderSpeech"
-        className="bubble bubble-bottom-left traderSpeech fade "
+        className={clsx({
+          displayNone: !props.dialogState, // if false
+          'bubble bubble-bottom-left traderSpeech fade': true, // always
+        })}
       >
         <div className="Speech-wrapper">
           <span
             className="TraderSpeechCancelBtn"
-            onClick={() => TraderSpeechHide()}
+            onClick={() => props.setDialogState(false)}
           >
             x
           </span>
-          <p> {props.traderDialogs}</p>
+          <p> {props.speech}</p>
         </div>
       </div>
       <div className="TraderImgContainer">
         <img className="Shop-trader-img" src="/images/ShopImg/trader.png" />
       </div>
-      <div className="Trader-speech-box"> Joney </div>
+      <div className="TraderName"> Joney </div>
     </div>
   )
 }
-
-export default Trader
