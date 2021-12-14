3ba1434e894a3069b4a52d31581002be7b445b70
Kristaps <azorien@gmail.com>
2021-12-14 15:59:22 +0200

    Add improved navigation bar image resources

--- packages/averill-client/src/components/Logo/Logo.jsx
+++ packages/averill-client/src/components/Logo/Logo.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { paths } from '../../router/routes'
-import logo from './logo.svg'
+import styles from './Logo.module.css'
 
 export default function Logo() {
   return (
     <>
-      <Link to={paths.home} className="hover:opacity-50 focus:opacity-50">
-        <img src={logo} width="50%" />
+      <Link to={paths.home}>
+        <div className={styles.logo} />
       </Link>
     </>
   )
