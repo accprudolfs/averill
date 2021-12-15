3c3a8c82f1d9ae743491e845257b1d3111f48c75
hedgethehog <micko.eleonora@gmail.com>
2021-12-15 11:27:56 +0200

    updates logo on hover

--- packages/averill-client/src/components/Logo/Logo.jsx
+++ packages/averill-client/src/components/Logo/Logo.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { paths } from '../../router/routes'
-import styles from './Logo.module.css'
+import logo from '../../../Public/images/logo.svg'
+import logoHovered from '../../../Public/images/logo_h.svg'
 
 export default function Logo() {
+  const [isHovering, setIsHovered] = useState(false)
+  const onMouseEnter = () => setIsHovered(true)
+  const onMouseLeave = () => setIsHovered(false)
   return (
-    <>
+    <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
       <Link to={paths.home}>
-        <div className={styles.logo} />
+        {isHovering ? <img src={logoHovered} /> : <img src={logo} />}
       </Link>
-    </>
+    </div>
   )
 }
